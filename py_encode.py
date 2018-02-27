9846edc26130dc162198bcb73c8ed74e98d7198a
Unknown <rhirons@gmail.com>
2018-02-27 13:55:46 +0000

    Initial commit

---
+++ py_encode.py
@@ -0,0 +1,45 @@
+# Simple script to batch convert video files in ffmpeg using hardware decoding & encoding
+# Script will process all video files in folder specificed in "mypath" and convert to h264 mkv files
+# The audio stream of the file will not be touched and simply copied. 
+# Ross Hirons
+from __future__ import print_function
+import subprocess
+from __builtin__ import input
+from os import getcwd, listdir
+from os.path import isfile, join, split
+
+# Can use raw string to hold the dir path or use os.path.join to normalise input. 
+# Forward slashes work in windows and works across Osx/linux.
+# Decided to user getcwd so you don't need to replace the path for every system..
+# just need to ensure files to convert are in a folder called batch in the dir of the python file.
+#mypath = r"E:/iCloudDrive/python_work/batch/"
+#mypath = join("E:/iCloudDrive/python_work/batch/")
+mypath = getcwd() + join("/batch")
+
+# Loops through "mypath" using os.listdir and if any files are found (isfile), the name is placed into list variable "onlyfiles". 
+onlyfiles = [x for x in listdir(mypath) if isfile(join(mypath, x))]
+
+# Add full dir path (mypath) to file name (onlyfiles) into it's own list (fullpath)
+fullpath = []
+for f in onlyfiles:
+    fullpath.append(mypath + f)
+
+print ("\n" + "=====================")
+print ("Files to convert are:")
+print ("=====================")
+
+# This shows the user the list of files that will be converted. 
+for f in onlyfiles:
+    print (f)
+print ("\n")
+
+# This constructs the ffmpeg command that will be passed to the windows shell. 
+# Using os.path.split to seperate the path and filename, Subprocess to execute the command
+# String.split used to remove the file extension as we want to append all output file as mkv
+for x in fullpath:
+    remove_extension = split(x)[1]
+    remove_extension = remove_extension.split('.')[0]
+    #print(remove_extension)
+    cmd = "ffmpeg.exe -hide_banner -hwaccel nvdec -i " + x + " -c:v h264_nvenc -crf 21 -c:a copy " + split(x)[0] + "/converted_" + remove_extension + ".mkv"
+    #print(cmd)
+    #subprocess.call(cmd, shell=True)
