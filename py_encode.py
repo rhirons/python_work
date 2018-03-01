02deca8900ef898de917270cab09817f87127e6b
Unknown <rhirons@gmail.com>
2018-03-01 17:11:58 +0000

    removed files

--- py_encode.py
+++ py_encode.py
@@ -1,42 +1,48 @@
-# Simple script to batch convert video files in ffmpeg using hardware decoding & encoding
-# Script will process all video files in folder specificed in "mypath" and convert to h264 mkv files
-# The audio stream of the file will not be touched and simply copied. 
-# Ross Hirons
+# Simple script to batch convert video files in ffmpeg using Nvidia hardware decoding & encoding.
+# Script will process all video files found in a folder called batch in the current directory
+# of the python script.
+
+# Video will be converted to h264 mkv file. Change to suit your needs.
+# The audio stream of the file will not be touched and simply copied.
+# Ross H - 2018
 from __future__ import print_function
 import subprocess
-import sys
+import platform
 from os import getcwd, listdir
 from os.path import isfile, join, split
-
 
 # Can use raw string to hold the dir path or use os.path.join to normalise input. 
 # Forward slashes work in windows and works across Osx/linux.
 # Decided to user getcwd so you don't need to replace the path for every system..
 # just need to ensure files to convert are in a folder called batch in the dir of the python file.
-#mypath = r"E:/iCloudDrive/python_work/batch/"
-#mypath = join("E:/iCloudDrive/python_work/batch/")
-mypath = getcwd() + "\\batch\\"
+#userpath = r"E:/iCloudDrive/python_work/batch/"
+#userpath = join("E:/iCloudDrive/python_work/batch/")
+userpath = getcwd() + "\\batch\\"
 
-# Loops through "mypath" using os.listdir and if any files are found (isfile), the name is placed into list variable "onlyfiles". 
-onlyfiles = [x for x in listdir(mypath) if isfile(join(mypath, x))]
+# Loops through "userpath" using os.listdir and if any files are found (isfile), the name is placed into list variable "filename". 
+filename = []
+filename = [x for x in listdir(userpath) if isfile(join(userpath, x))]
 
-# Add full dir path (mypath) to file name (onlyfiles) into it's own list (fullpath)
+# Add full dir path (userpath) to file name (filename) into it's own list (fullpath)
 fullpath = []
-for f in onlyfiles:
-    fullpath.append(mypath + f)
+for f in filename:
+    fullpath.append(userpath + f)
 
+#Determines which OS user is running.
+which_os = platform.system()
+print("OS Detected is: " + which_os + "\n")
 
+# Asks user if they want to convert sequentially or in parallel. Will be used in the if statement later on
 user_choice = raw_input("Do you want to convert one at a time? Y/N: ")
 user_choice = user_choice.upper()
+#print (user_choice)
 
-
-print (user_choice)
 print ("\n" + "=====================")
 print ("Files to convert are:")
 print ("=====================")
 
 # This shows the user the list of files that will be converted. 
-for f in onlyfiles:
+for f in filename:
     print (f)
 print ("\n")
 
@@ -46,9 +52,9 @@ print ("\n")
 for x in fullpath:
     remove_extension = split(x)[1]
     remove_extension = remove_extension.split('.')[0]
-    #print(remove_extension)
     cmd = "ffmpeg.exe -hide_banner -hwaccel nvdec -i " + x + " -c:v h264_nvenc -crf 22 -c:a copy " + split(x)[0] + "\\converted_" + remove_extension + ".mkv"
     print(cmd)
+    
     #Un-comment the below line when you're ready to convert. I've commented it out for now as I'm still making changes.
     #Use subprocess.call if you want to convert one by one.
     #Use subprocess.Popen if you want to convert everything at once.
